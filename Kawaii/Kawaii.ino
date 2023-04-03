93078b41f387c75923e5b5bb6042e9c1a3f6f208
Maruyama <taku2000maru@icloud.com>
2023-04-03 15:39:16 +0900

    func

--- Kawaii/Kawaii.ino
+++ Kawaii/Kawaii.ino
@@ -1,9 +1,38 @@
+#include <M5Core2.h>
+
 void setup() {
   // put your setup code here, to run once:
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (M5.BtnA.wasPressed()) {
+  }
+  if (M5.BtnB.wasPressed()) {
+  }
+  if (M5.BtnC.wasPressed()) {
+  }
 }
+
+
+
+class Kakurenbo(){
+
+};
+class Darumasan(){
+  
+};
+
+class FaseControl {
+
+  void face_draw() {
+    //表情を描画する
+  }
+
+  void eye() {
+    //目
+  }
+
+  void mouth() {
+  }
+};
