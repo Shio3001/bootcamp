f14bf9954853a7e67b85c6496edb0d9e05f5c252
Maruyama <taku2000maru@icloud.com>
2023-04-03 16:32:44 +0900

    make class2

--- Kawaii/Kawaii.ino
+++ Kawaii/Kawaii.ino
@@ -1,30 +1,58 @@
 #include <M5Core2.h>
 
+
+class GameClass {
+private:
+public:
+  virtual void game_loop() const;
+};
+
+class Kakurenbo : public GameClass {
+public:
+  void game_loop() const override{
+
+  };
+};
+class Darumasan : public GameClass {
+public:
+  void game_loop() const override{
+
+  };
+};
+
+
+int gamemode = 0;
+GameClass *game_class = nullptr;
+
 void setup() {
   // put your setup code here, to run once:
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  if (M5.BtnA.wasPressed()) {
-  }
-  if (M5.BtnB.wasPressed()) {
-  }
-  if (M5.BtnC.wasPressed()) {
+void loop() {  //ボタンの入力処理用
+  switch (gamemode) {
+    case 0:
+      gamemode = gamemode_select();
+      break;
+    case 1:  //かくれんぼ
+      break;
+    case 2:  //ダルマさん
+      break;
   }
 }
 
-
-
-class Kakurenbo(){
-
-};
-class Darumasan(){
-  
-};
+int gamemode_select() {
+  if (M5.BtnA.wasPressed()) {  //かくれんぼに誘導
+    game_class = new Kakurenbo();
+    return 1;
+  }
+  if (M5.BtnB.wasPressed()) {  //ダルマさんが転んだに誘導
+    game_class = new Darumasan();
+    return 2;
+  }
+  return 0;
+}
 
 class FaseControl {
-
   void face_draw() {
     //表情を描画する
   }
